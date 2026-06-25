51121f1e581235338a13c518da1c9073ab86dec5
hassanmegahed71291-beep <hassanmegahed71291@gmail.com>
2026-06-25 16:06:09 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-async function getAnswer(userQuestion) {
-  const response = await fetch('database.json');
-  const data = await response.json();
-  
-  const match = data.questions.find(q => userQuestion.includes(q.question));
-  return match ? match.answer : "السؤال غير موجود في القاعدة.";
-}
